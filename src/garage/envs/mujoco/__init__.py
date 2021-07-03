e3ccb3fbc761242daf5a0b79d933331df63c3b6d
Luckeciano Melo <luckeciano@gmail.com>
2021-07-02 18:46:03 -0700

    Adding AntGoalEnv

--- src/garage/envs/mujoco/__init__.py
+++ src/garage/envs/mujoco/__init__.py
@@ -8,10 +8,12 @@ from garage.envs.mujoco.half_cheetah_dir_env import HalfCheetahDirEnv
 from garage.envs.mujoco.half_cheetah_vel_env import HalfCheetahVelEnv
 from garage.envs.mujoco.ant_dir import AntDirEnv
 from garage.envs.mujoco.humanoid_dir import HumanoidDirEnv
+from garage.envs.mujoco.ant_goal import AntGoalEnv
 
 __all__ = [
     'HalfCheetahDirEnv',
     'HalfCheetahVelEnv',
     'AntDirEnv',
-    'HumanoidDirEnv'
+    'HumanoidDirEnv',
+    'AntGoalEnv'
 ]
