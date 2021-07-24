b792750377e354fc494c0f26453c533e8fbb88e8
Luckeciano Melo <luckeciano@gmail.com>
2021-07-24 00:28:28 -0700

    Adding push and pick place in init

--- src/garage/envs/metaworld/__init__.py
+++ src/garage/envs/metaworld/__init__.py
@@ -1,6 +1,8 @@
-from garage.envs.metaworld.ml1 import ML1Env, ML1ReachEnv
+from garage.envs.metaworld.ml1 import ML1Env, ML1ReachEnv, ML1PushEnv, ML1PickPlaceEnv
 
 __all__ = [
     'ML1Env',
-    'ML1ReachEnv'
+    'ML1ReachEnv',
+    'ML1PushEnv',
+    'ML1PickPlaceEnv'
 ]
